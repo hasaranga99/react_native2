d21daba29f20f5fc3e474c2e83d758d756ee3a9e
W.G.R Hasaranga <106307238+hasaranga99@users.noreply.github.com>
2024-08-07 12:04:29 +0530

    text field

--- App.tsx
+++ App.tsx
@@ -1,42 +1,41 @@
 import React, { useState } from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, Text, TextInput, View} from 'react-native';
 import CustomLabel from './src/components/CustomLabel';
 
 
 
-
-function Click (x:number ,d:number){
-  
-  console.log("Addition is :" +(x+d));
-  console.log("% is :" +(x%d));
-}
-
 function App(): React.JSX.Element {
   return (
     <View style={{alignItems: 'center'}}>
-      <CustomText fsize={40} a='React' b='Native' />
-    </View>
-  );
-}
-
-type CustomTextProps = {
-  fsize?:number
-  a:string
-  b?:string
-};
-
-function CustomText(props: CustomTextProps) {
-  return (
-    <View>
-    <Text style={{fontSize: props.fsize}}>
-      {props.a} {props.b}
-    </Text>
-
-
-    <Button title='Click Me' onPress={()=>{
-      Click(10,50)
-    }}/>
-    
+      
+      
+      <TextInput style={{
+        backgroundColor:'#aaa',
+        color:'#000',
+        width:300,
+        marginTop:150,
+        borderRadius:10,
+        paddingLeft:30,
+        paddingRight:30,
+      }} 
+      placeholder='User Name'
+      placeholderTextColor={'#444'}
+      
+      />
+      <TextInput style={{
+        backgroundColor:'#aaa',
+        color:'#000',
+        width:300,
+        marginTop:20,
+        borderRadius:10,
+        paddingLeft:30,
+        paddingRight:30,
+      }} 
+      secureTextEntry={true}
+      placeholder='Password'
+      placeholderTextColor={'#444'}
+      
+      />
     </View>
   );
 }
