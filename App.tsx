856a329acdc8dddfe8b0e5377a39132d40906ff0
W.G.R Hasaranga <106307238+hasaranga99@users.noreply.github.com>
2024-08-08 00:14:50 +0530

    object

--- App.tsx
+++ App.tsx
@@ -1,35 +1,51 @@
 import React, { useState } from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
+import { Alert, Button, Text, TextInput, View } from 'react-native';
 import CustomLabel from './src/components/CustomLabel';
 
 
 
 function App(): React.JSX.Element {
 
-  var textValue = ''
-
-  function ImageCatch1() {
-
-    console.log("Test_Test")
-
+  var Student = {
+    name: 'jenny',
+    age: 6,
+    school: 'Abc School',
+    eat: function () {
+      console.log("Eating...")
+    },
+    play: function () {
+      console.log("Play some games...")
+    }
   }
 
-  function DisplayValue() {
-    console.log('Text Value = ' + textValue)
+
+
+  var Cat = {
+    name: 'kity',
+    color: 'white',
+    sleep: function () {
+      console.log("Purrrr.....");
+    }
   }
 
-  function OnTextChange(v: string) {
-    textValue = v;
+  function makeStudentEat() {
+    Student.eat();
   }
+
+  function makeCatSleep() {
+    Cat.sleep();
+  }
+
   return (
-    <View style={{ alignItems: 'center', marginTop: 40 }}>
-      <TextInput style={{
-        width: 300, height: 50, backgroundColor: 'black', color: 'white', borderRadius: 40,
-        marginVertical: 20, paddingHorizontal: 20, padding: 5
-
-      }} onChangeText={OnTextChange} />
-
-      <Button title='Click here' onPress={DisplayValue} />
+    <View style={{ alignItems: 'center', flex: 1, backgroundColor: 'white', justifyContent: 'center' }}>
+      <Text style={{ fontSize: 30, color: '#444' }}>Student</Text>
+      <Text style={{ fontSize: 30, color: '#444' }}>{Student.name}</Text>
+      <Text style={{ fontSize: 30, color: '#444' }}>{Student.age}</Text>
+      <Button title='Student Eat' onPress={makeStudentEat} />
+      <Text style={{ fontSize: 30, color: '#444' }}>Cat</Text>
+      <Text style={{ fontSize: 30, color: '#444' }}>{Cat.name}</Text>
+      <Text style={{ fontSize: 30, color: '#444' }}>{Cat.color}</Text>
+      <Button title='Cat Sleep' onPress={makeCatSleep} />
     </View>
   );
 
