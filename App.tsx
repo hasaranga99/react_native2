dee0d9e8bea9483932af4ff88f32e0fd9bdbc7d7
W.G.R Hasaranga <106307238+hasaranga99@users.noreply.github.com>
2024-08-07 14:20:49 +0530

    use state statement eka hadana vidiha

--- App.tsx
+++ App.tsx
@@ -1,43 +1,29 @@
 import React, { useState } from 'react';
-import {Button, Text, TextInput, View} from 'react-native';
+import { Button, Text, TextInput, View } from 'react-native';
 import CustomLabel from './src/components/CustomLabel';
 
 
 
 function App(): React.JSX.Element {
+
+  console.log('App Component is rendered');
+
+
+  const [count, setCount] = useState(0);
+
+  function ChangeFunction() {
+    setCount(count + 1);
+  }
+
+
   return (
-    <View style={{alignItems: 'center'}}>
-      
-      
-      <TextInput style={{
-        backgroundColor:'#aaa',
-        color:'#000',
-        width:300,
-        marginTop:150,
-        borderRadius:10,
-        paddingLeft:30,
-        paddingRight:30,
-      }} 
-      placeholder='User Name'
-      placeholderTextColor={'#444'}
-      
-      />
-      <TextInput style={{
-        backgroundColor:'#aaa',
-        color:'#000',
-        width:300,
-        marginTop:20,
-        borderRadius:10,
-        paddingLeft:30,
-        paddingRight:30,
-      }} 
-      secureTextEntry={true}
-      placeholder='Password'
-      placeholderTextColor={'#444'}
-      
-      />
+    <View style={{ alignItems: 'center', marginTop: 40 }}>
+      <Text style={{ fontSize: 30 }}>Count is :{count}</Text>
+      <Button title='Change Count' onPress={ChangeFunction} />
     </View>
   );
+
 }
+
 
 export default App;
