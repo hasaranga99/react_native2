73c11be2f2274137c26de223cbc6e6d3d89b59d0
W.G.R Hasaranga <106307238+hasaranga99@users.noreply.github.com>
2024-08-08 22:15:07 +0530

    class introduction

--- App.tsx
+++ App.tsx
@@ -6,46 +6,78 @@ import CustomLabel from './src/components/CustomLabel';
 
 function App(): React.JSX.Element {
 
-  var Student = {
-    name: 'jenny',
-    age: 6,
-    school: 'Abc School',
-    eat: function () {
-      console.log("Eating...")
-    },
-    play: function () {
-      console.log("Play some games...")
+  class Cal {
+    number1: number = 0;
+    number2: number = 0;
+    Add() {
+      console.log(this.number1 + this.number2)
+    }
+    Sub() {
+      console.log(this.number1 - this.number2)
     }
   }
 
+  function solve() {
+    var cal1 = new Cal();
+    cal1.number1 = 10;
+    cal1.number2 = 5;
+    cal1.Add();
+
+    var cal2 = new Cal();
+    cal1.number1 = 10;
+    cal1.number2 = 5;
+    cal1.Sub();
 
 
-  var Cat = {
-    name: 'kity',
-    color: 'white',
-    sleep: function () {
-      console.log("Purrrr.....");
-    }
   }
 
-  function makeStudentEat() {
-    Student.eat();
-  }
+  // var cal = {
+  //   number1: 0,
+  //   number2: 0,
+  //   add: function () {
+  //     console.log(this.number1 + this.number2)
+  //   },
+  //   sub: function () {
+  //     console.log(this.number1 - this.number2)
+  //   },
+  //   mul: function () {
+  //     console.log(this.number1 * this.number2)
+  //   },
+  //   dev: function () {
+  //     console.log(this.number1 / this.number2)
+  //   }
+  // }
 
-  function makeCatSleep() {
-    Cat.sleep();
-  }
+
+
+
+  // function adding() {
+  //   cal.number1 = 30;
+  //   cal.number2 = 50;
+  //   cal.add();
+  // }
+
+  // function subing() {
+  //   cal.number1 = 30;
+  //   cal.number2 = 50;
+  //   cal.sub();
+  // }
+  // function multi() {
+  //   cal.number1 = 30;
+  //   cal.number2 = 50;
+  //   cal.mul();
+  // }
+  // function devi() {
+  //   cal.number1 = 30;
+  //   cal.number2 = 50;
+  //   cal.dev();
+  // }
 
   return (
-    <View style={{ alignItems: 'center', flex: 1, backgroundColor: 'white', justifyContent: 'center' }}>
-      <Text style={{ fontSize: 30, color: '#444' }}>Student</Text>
-      <Text style={{ fontSize: 30, color: '#444' }}>{Student.name}</Text>
-      <Text style={{ fontSize: 30, color: '#444' }}>{Student.age}</Text>
-      <Button title='Student Eat' onPress={makeStudentEat} />
-      <Text style={{ fontSize: 30, color: '#444' }}>Cat</Text>
-      <Text style={{ fontSize: 30, color: '#444' }}>{Cat.name}</Text>
-      <Text style={{ fontSize: 30, color: '#444' }}>{Cat.color}</Text>
-      <Button title='Cat Sleep' onPress={makeCatSleep} />
+    <View style={{ alignItems: 'center', flex: 1, backgroundColor: 'white', justifyContent: 'center', display: 'flex' }}>
+      <Text style={{ fontSize: 30, color: '#444' }}>ADD</Text>
+
+      <Button title='Add/sub/mul/dev' onPress={solve} />
     </View>
   );
 
