e4b7736271c26dedf199dc59bf40a56dd586da92
W.G.R Hasaranga <106307238+hasaranga99@users.noreply.github.com>
2024-08-09 13:22:34 +0530

    lest

--- App.tsx
+++ App.tsx
@@ -1,33 +1,101 @@
 import React, { useState } from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
+import { Alert, Button, Text, TextInput, View } from 'react-native';
 import CustomLabel from './src/components/CustomLabel';
 
 
 
 function App(): React.JSX.Element {
 
-  var textValue = ''
-
-  function ImageCatch() {
-
+  interface Student {
+    name: string
+    age: number
+    school: string
   }
 
-  function DisplayValue() {
-    console.log('Text Value = ' + textValue)
+  var Student_1: Student = {
+    name: 'ravi',
+    age: 25,
+    school: 'ABC SChool'
   }
 
-  function OnTextChange(v: string) {
-    textValue = v;
+  function printData(std: Student) {
+    console.log(std.name);
+    console.log(std.age);
+    console.log(std.school)
   }
+
+  // class Cal {
+  //   number1: number = 0;
+  //   number2: number = 0;
+  //   Add() {
+  //     console.log(this.number1 + this.number2)
+  //   }
+  //   Sub() {
+  //     console.log(this.number1 - this.number2)
+  //   }
+  // }
+
+  // function solve() {
+  //   var cal1 = new Cal();
+  //   cal1.number1 = 10;
+  //   cal1.number2 = 5;
+  //   cal1.Add();
+
+  //   var cal2 = new Cal();
+  //   cal1.number1 = 10;
+  //   cal1.number2 = 5;
+  //   cal1.Sub();
+
+
+  // }
+
+  // var cal = {
+  //   number1: 0,
+  //   number2: 0,
+  //   add: function () {
+  //     console.log(this.number1 + this.number2)
+  //   },
+  //   sub: function () {
+  //     console.log(this.number1 - this.number2)
+  //   },
+  //   mul: function () {
+  //     console.log(this.number1 * this.number2)
+  //   },
+  //   dev: function () {
+  //     console.log(this.number1 / this.number2)
+  //   }
+  // }
+
+
+
+
+  // function adding() {
+  //   cal.number1 = 30;
+  //   cal.number2 = 50;
+  //   cal.add();
+  // }
+
+  // function subing() {
+  //   cal.number1 = 30;
+  //   cal.number2 = 50;
+  //   cal.sub();
+  // }
+  // function multi() {
+  //   cal.number1 = 30;
+  //   cal.number2 = 50;
+  //   cal.mul();
+  // }
+  // function devi() {
+  //   cal.number1 = 30;
+  //   cal.number2 = 50;
+  //   cal.dev();
+  // }
+
   return (
-    <View style={{ alignItems: 'center', marginTop: 40 }}>
-      <TextInput style={{
-        width: 300, height: 50, backgroundColor: 'black', color: 'white', borderRadius: 40,
-        marginVertical: 20, paddingHorizontal: 20, padding: 5
+    <View style={{ alignItems: 'center', flex: 1, backgroundColor: 'white', justifyContent: 'center', display: 'flex' }}>
+      <Text style={{ fontSize: 30, color: '#444' }}>ADD</Text>
 
-      }} onChangeText={OnTextChange} />
-
-      <Button title='Click here' onPress={DisplayValue} />
+      <Button title='Add/sub/mul/dev' onPress={() => printData(Student_1)} />
     </View>
   );
 
