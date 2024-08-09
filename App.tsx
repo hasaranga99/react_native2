bf1fe9299cc28d4e27fd038bc2957a78a0f9d70e
W.G.R Hasaranga <106307238+hasaranga99@users.noreply.github.com>
2024-08-09 14:58:02 +0530

    flex include style

--- App.tsx
+++ App.tsx
@@ -6,97 +6,40 @@ import CustomLabel from './src/components/CustomLabel';
 
 function App(): React.JSX.Element {
 
-  interface Student {
-    name: string
-    age: number
-    school: string
-  }
 
-  var Student_1: Student = {
-    name: 'ravi',
-    age: 25,
-    school: 'ABC SChool'
-  }
-
-  function printData(std: Student) {
-    console.log(std.name);
-    console.log(std.age);
-    console.log(std.school)
-  }
-
-  // class Cal {
-  //   number1: number = 0;
-  //   number2: number = 0;
-  //   Add() {
-  //     console.log(this.number1 + this.number2)
-  //   }
-  //   Sub() {
-  //     console.log(this.number1 - this.number2)
-  //   }
-  // }
-
-  // function solve() {
-  //   var cal1 = new Cal();
-  //   cal1.number1 = 10;
-  //   cal1.number2 = 5;
-  //   cal1.Add();
-
-  //   var cal2 = new Cal();
-  //   cal1.number1 = 10;
-  //   cal1.number2 = 5;
-  //   cal1.Sub();
-
-
-  // }
-
-  // var cal = {
-  //   number1: 0,
-  //   number2: 0,
-  //   add: function () {
-  //     console.log(this.number1 + this.number2)
-  //   },
-  //   sub: function () {
-  //     console.log(this.number1 - this.number2)
-  //   },
-  //   mul: function () {
-  //     console.log(this.number1 * this.number2)
-  //   },
-  //   dev: function () {
-  //     console.log(this.number1 / this.number2)
-  //   }
-  // }
-
-
-
-
-  // function adding() {
-  //   cal.number1 = 30;
-  //   cal.number2 = 50;
-  //   cal.add();
-  // }
-
-  // function subing() {
-  //   cal.number1 = 30;
-  //   cal.number2 = 50;
-  //   cal.sub();
-  // }
-  // function multi() {
-  //   cal.number1 = 30;
-  //   cal.number2 = 50;
-  //   cal.mul();
-  // }
-  // function devi() {
-  //   cal.number1 = 30;
-  //   cal.number2 = 50;
-  //   cal.dev();
-  // }
 
   return (
-    <View style={{ alignItems: 'center', flex: 1, backgroundColor: 'white', justifyContent: 'center', display: 'flex' }}>
-      <Text style={{ fontSize: 30, color: '#444' }}>ADD</Text>
+    // <View style={{ backgroundColor: 'yellow', flex: 1, flexDirection: 'column' }}>
 
-      <Button title='Add/sub/mul/dev' onPress={() => printData(Student_1)} />
+    //   <View style={{ backgroundColor: 'black', flex: 1 }}>
+    //   </View>
+    //   <View style={{ backgroundColor: 'green', flex: 1.5, flexDirection: 'row' }}>
+    //     <View style={{ backgroundColor: 'red', flex: 1 }}></View>
+    //     <View style={{ backgroundColor: 'yellow', flex: 1, flexDirection: 'column' }}>
+    //       <View style={{ backgroundColor: 'green', flex: 1 }}></View>
+    //       <View style={{ backgroundColor: 'yellow', flex: 1 }}></View>
+    //     </View>
+
+    //   </View>
+    // </View>
+
+    <View style={{ backgroundColor: 'yellow', flex: 1 }}>
+      <View style={{ backgroundColor: 'black', flex: 2 }}></View>
+      <View style={{ backgroundColor: 'red', flex: 3, flexDirection: 'row' }}>
+        <View style={{ backgroundColor: 'red', flex: 1 }}></View>
+        <View style={{ backgroundColor: 'green', flex: 1, flexDirection: 'column' }}>
+          <View style={{ backgroundColor: 'green', flex: 1, flexDirection: 'row' }}>
+            <View style={{ backgroundColor: '#000', flex: 1 }}></View>
+            <View style={{ backgroundColor: '#666', flex: 1 }}></View>
+            <View style={{ backgroundColor: '#fff', flex: 1 }}></View>
+
+          </View>
+          <View style={{ backgroundColor: 'yellow', flex: 1 }}></View>
+        </View>
+      </View>
+      <View style={{ backgroundColor: 'blue', flex: 2 }}></View>
     </View>
+
   );
 
 }
